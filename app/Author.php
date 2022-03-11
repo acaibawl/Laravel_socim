27fda694725924906779063daf5da0186c142615
acaibawl <hagiya0719@gmail.com>
2022-03-11 21:36:42 +0000

    リレーションを試す

--- app/Author.php
+++ app/Author.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Author extends Model
 {
     use SoftDeletes;
-    
+
     public function getKanaAttribute(string $value): string
     {
         return mb_convert_kana($value, 'k');
@@ -17,5 +17,10 @@ class Author extends Model
     public function setKanaAttribute(string $value)
     {
         $this->attributes['kana'] = mb_convert_kana($value, 'KV');
+    }
+
+    public function books()
+    {
+        return $this->hasMany('\App\Book');
     }
 }
