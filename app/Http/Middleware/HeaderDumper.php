3242c4a379fa36c4da7a7c3549f90b11174a906d
acaibawl <hagiya0719@gmail.com>
2022-03-11 19:39:44 +0000

    リクエストとレスポンスのHeaderをログ出力するmiddleware作成

---
+++ app/Http/Middleware/HeaderDumper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Psr\Log\LoggerInterface;
+
+class HeaderDumper
+{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request instanceof Request) {
+            $this->logger->info('request', [
+                'header' => strval($request->headers),
+            ]);
+        }
+        $response = $next($request);
+        if ($response instanceof Response) {
+            $this->logger->info('response', [
+                'header' => strval($response->headers),
+            ]);
+        }
+        
+        return $response;
+    }
+}
