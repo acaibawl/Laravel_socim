9e031ffbdd34f75da1631d5ab2b89fbd09b0f1f6
acaibawl <hagiya0719@gmail.com>
2022-03-11 15:32:09 +0000

    AppServiceProviderで、暗号化のプロバイダをBlowfishに変更してみる

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\BlowfishEncrypter;
+use Illuminate\Support\Str;
 use Illuminate\Support\ServiceProvider;
+use RuntimeException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('encrypter', function ($app) {
+            $config = $app->make('config')->get('app');
+
+            if (Str::startsWith($key = $this->key($config), 'base64:')) {
+                $key = base64_decode(substr($key, 7));
+            }
+            return new BlowfishEncrypter($key);
+        });
+    }
+
+    protected function key(array $config)
+    {
+        return tap($config['key'], function ($key) {
+            if (empty($key)) {
+                throw new RuntimeException(
+                    'No application encryption key has been specified.'
+                );
+            }
+        });
     }
 }
