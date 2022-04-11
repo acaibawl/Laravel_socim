369dba0aa35ab21dd99f6d8051b5c5ac016a072d
acaibawl <hagiya0719@gmail.com>
2022-04-11 16:23:46 +0900

    controllerの内容をserviceに切り出し

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,21 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Customer;
+use App\Services\CustomerService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class ApiController extends Controller
 {
-    public function getCustomers()
+    public function getCustomers(CustomerService $service)
     {
-        return response()->json(Customer::query()->select(['id', 'name'])->get());
+        return response()->json($service->getCustomers());
     }
 
-    public function postCustomers(Request $request)
+    public function postCustomers(Request $request, CustomerService $service)
     {
         $this->validate($request, ['name' => 'required']);
-        $customer = new Customer();
-        $customer->name = $request->json('name');
-        $customer->save();
+        $service->addCustomer($request->json('name'));
     }
 }
